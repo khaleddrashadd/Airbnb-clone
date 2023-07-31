52954858afd86d13ba3e8f0e7c0398c59e7543c2
khaleddrashadd <khaledrashad111@gmail.com>
2023-07-31 06:57:34 +0300

    finished reservation page

---
+++ app/reservations/page.jsx
@@ -0,0 +1,34 @@
+import { EmptyState, ReservationsClient } from '@/components';
+import { getCurrentUser } from '../actions';
+import { getReservations } from '../actions/getReservations';
+
+const ReservationsPage = async () => {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return (
+      <EmptyState
+        title="Unauthorized"
+        subtitle="Please login"
+      />
+    );
+  }
+  const authorId = currentUser.id;
+  const reservations = await getReservations(authorId);
+  console.log(reservations,"💥✔️")
+  if (!reservations.length) {
+    return (
+      <EmptyState
+        title="No reservations found"
+        subtitle="You have no reservations"
+      />
+    );
+  }
+
+  return (
+    <ReservationsClient
+      reservations={reservations}
+      currentUser={currentUser}
+    />
+  );
+};
+export default ReservationsPage;
---
+++ components/reservations/ReservationsClient.jsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { Container, Heading, ListingCard } from '..';
+import { useState } from 'react';
+import { toast } from 'react-hot-toast';
+
+const ReservationsClient = ({ reservations, currentUser }) => {
+  const [deletingId, setDeletingId] = useState('');
+  const router = useRouter();
+
+  const handleCancel = id => {
+    setDeletingId(id);
+    removeData('reservations', id)
+      .then(res => {
+        toast.success('Reservation deleted successfully', { duration: 2000 });
+        router.refresh();
+      })
+      .catch(err => {
+        toast
+          .error(err || 'Error deleting reservation', { duration: 2000 })
+          .finally(() => setDeletingId(''));
+      });
+  };
+
+  return (
+    <Container>
+      <Heading
+        title="Reservations"
+        subtitle="Booking on your properties"
+      />
+      <div className="mt-10 flex flex-wrap items-center justify-center sm:justify-start gap-8">
+        {reservations.map(reservation => (
+          <ListingCard
+            key={reservation.id}
+            data={reservation.listing}
+            reservation={reservation}
+            actionId={reservation.id}
+            actionLabel="Cancel reservation"
+            disabled={deletingId === reservation.id}
+            onAction={handleCancel}
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+export default ReservationsClient;
